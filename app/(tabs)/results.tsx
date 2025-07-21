940485b292623351ef8ae3a453416745b77b980c
Sav <maidenfan78@gmail.com>
2025-07-21 21:41:32 +1000

    bug fix

--- app/(tabs)/results.tsx
+++ app/(tabs)/results.tsx
@@ -1,15 +1,16 @@
 import { Text, View } from 'react-native';
-import { Stack } from 'expo-router'; 
 import { useLocalSearchParams } from 'expo-router';
+
+export const config = {
+  title: 'Scan Results',
+};
 
 export default function Results() {
   const { code } = useLocalSearchParams<{ code: string }>();
 
   return (
     <View style={{ flex: 1, padding: 20 }}>
-      <Stack.Screen options={{ title: 'Scan Results' }} />  {/* configure header */}
       <Text>Scanned code: {code}</Text>
-      {/* Later: send code to backend, parse and show product info */}
     </View>
   );
 }
